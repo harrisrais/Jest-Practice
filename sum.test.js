c273a70de0fd0a81c469bda58110abaf0b38fabe
Haris Raees <haris.pixako1@gmail.com>
2025-10-23 12:18:42 +0500

    used toBeFalsy matcher to check if value is blank, null, undefined, zero false

--- sum.test.js
+++ sum.test.js
@@ -7,15 +7,20 @@
 //     expect(compare('Haris', 'Haris')).toBe(true);
 // })
 
-test('object assignement', () => {
-    const data = {one: 1};
-     data['two'] = 2;
-    expect(data).toEqual({one: 1, two: 2});
-})
+// test('object assignement', () => {
+//     const data = {one: 1};
+//      data['two'] = 2;
+//      console.log("data:: ", data);
+//     expect(data).toEqual({one: 1, two: 2});
+// });
 
-
+test('null is falsy', () => {
+    const n = null;
+    expect(n).toBeFalsy();
+});
 
 /*matchers
 > toBe is used for primitive values (numbers, strings and booleans)
 > toEqual is used when comparing the values of objects or arrays
+> toBeFalsy is used to check if value is null, undefined, blank, zero, false
 */
