5d688b7497709cbe0b603c8ddacd21dab5d799f5
Haris Raees <haris.pixako1@gmail.com>
2025-10-27 16:29:25 +0500

    minor change

--- sum.test.js
+++ sum.test.js
@@ -55,10 +55,23 @@ const fetchPromise = require('./sum')
 //     expect(data).toBe('peanut butter');
 // })
 
-test('mock implementation of a basic function', () => {
-    const mock = jest.fn(x => 42 + x);
-    expect(mock(2)).toBe(44);
-    expect(mock).toHaveBeenCalledWith(2);
+// test('mock implementation of a basic function', () => {
+//     const mock = jest.fn(x => 42 + x);
+//     expect(mock(2)).toBe(44);
+//     expect(mock).toHaveBeenCalledWith(2);
+// })
+
+test('spying on a  method of an object', () => {
+    const video = {
+        play() {
+            return true;
+        }
+    }
+    const spy = jest.spyOn(video, 'play');
+    video.play();
+
+    expect(spy).toHaveBeenCalled();
+    spy.mockRestore();
 })
 
 /* MATCHERS
